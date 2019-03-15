d2fbb5dba338bd1da9aa4991adbdf495835afdee
ugiriwabo <ugiraconsole@gmail.com>
2019-03-15 10:41:08 +0200

    the form of login displayed and you can put the username and password and the you press login

--- news/models.py
+++ news/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
 # Create your models here.
+  
+class Category (models.Model):
+    name = models.CharField(max_length=30)
+
+class Location (models.Model): 
+    name = models.CharField(max_length=30) 
 
 class Image(models.Model):
     title = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=1024)
-    # image_location = db.Column(db.Integer,db.ForeignKey("image_location"))
-    # image_category = db.Column(db.Integer,db.ForeignKey("image_category"))
-    
-  
-class Category (models.Model):
-    title = models.CharField(max_length=30)
-    name = models.CharField(max_length=30)
-
-class Location (models.Model): 
-    title = models.CharField(max_length=30)
-    name = models.CharField(max_length=30) 
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
