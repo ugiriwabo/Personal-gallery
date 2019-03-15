bce516e032889d254fdca8bf0668e308c949b40e
ugiriwabo <ugiraconsole@gmail.com>
2019-03-15 09:25:52 +0200

    add Category and Location in models

--- news/models.py
+++ news/models.py
@@ -6,8 +6,14 @@ class Image(models.Model):
     title = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=1024)
-    image_location = db.Column(db.Integer,db.ForeignKey("image_location"))
-    image_category = db.Column(db.Integer,db.ForeignKey("image_category"))
+    # image_location = db.Column(db.Integer,db.ForeignKey("image_location"))
+    # image_category = db.Column(db.Integer,db.ForeignKey("image_category"))
+    
+  
+class Category (models.Model):
+    title = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
-   
-        
+class Location (models.Model): 
+    title = models.CharField(max_length=30)
+    name = models.CharField(max_length=30) 
--- news/views.py
+++ news/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
 
-
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
